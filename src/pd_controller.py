350c45bfd0bd92d3fa853f88122eb91f43b416a0
zlt1213 <zlt1213@gmail.com>
2017-11-14 21:58:20 +0800

    pd controller init

---
+++ src/pd_controller.py
@@ -0,0 +1,48 @@
+# ==== this is the class of pd controller ====
+
+class PD_controller:
+
+    def __init__(sefl, kp = 0.0, kd = 0.0, start_time = 0):
+
+        # set the init value of kp and kd
+        self.kp_ = float(kp)
+        self.kd_ = float(kd)
+
+        # define the laster_error_ and set to 0.0
+        self.laster_error_ = 0.0
+
+        # store relevant data
+        self.last_timestamp_ = 0.0
+        self.set_point_ = 0.0
+        self.start_time_ = start_time
+        self.error_sum_ = 0.0
+
+        # control effort history
+        self.u_p = [0]
+        self.u_d = [0]
+
+    def setTarget(self, target):
+        self.set_point_ = float(target)
+
+    def setKP(self, kp):
+        self.kp_ = float(kp)
+
+    def setKD(self, kd):
+        self.kd_ = float(kd)
+
+    def update(self, measured_value, timestamp):
+        delta_time = timestamp - self.last_timestamp_
+        if delta_time == 0:
+            # Delta time is zero
+            return 0
+
+
+
+
+
+
+
+
+
+
+        
